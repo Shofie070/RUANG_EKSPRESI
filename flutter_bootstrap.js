27e528aa5a6b8f2b308ac89450dd44d2f20afaa4
Shofie070 <24111814070@mhs.unesa.ac.id>
2025-10-22 09:44:36 +0700

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "634887066"
+    serviceWorkerVersion: "2482177848"
   }
 });
